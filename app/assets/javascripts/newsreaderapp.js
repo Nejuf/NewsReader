41e8d01cd307643382537f7b322fdaa0b1954ff9
App Academy Student <student@appacademy.io>
2013-11-01 10:48:27 -0700

    Initial commit

---
+++ app/assets/javascripts/newsreaderapp.js
@@ -0,0 +1,13 @@
+window.Newsreaderapp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Newsreaderapp.initialize();
+});
