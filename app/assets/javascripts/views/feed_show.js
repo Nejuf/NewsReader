ad42521ef93e5cdc59be8953fcd6c3ed8ce8509b
App Academy Student <student@appacademy.io>
2013-11-01 12:20:52 -0700

    Adds feed entry refresh button

--- app/assets/javascripts/views/feed_show.js
+++ app/assets/javascripts/views/feed_show.js
@@ -1,9 +1,25 @@
 NewsReader.Views.FeedShow = Backbone.View.extend({
 	template: JST["feeds/show"],
 
+	events: {
+		"click .refresh-entries": "reloadEntries"
+	},
+
 	render: function(){
 		var renderedContent = this.template({ feed: this.model });
 		this.$el.html(renderedContent);
 		return this;
 	},
+
+	reloadEntries: function(event) {
+		var entries = $.ajax({
+			type: "GET",
+			url: "feeds/" + this.model.id + "/entries",
+			datatype: "json",
+			success: function(data, textStatus, jqXHR) {
+				debugger
+			}
+		})
+	}
+
 });
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,10 +2,10 @@ class EntriesController < ApplicationController
   def index
     feed = Feed.find(params[:feed_id])
 
-    if(feed && feed.updated_at < Time.now - 120)
+    if(feed && (feed.updated_at < (Time.now - 120) || feed.entries.length == 0))
       feed.reload
+      debugger
     end
-
     render :json => feed.entries
   end
 end
